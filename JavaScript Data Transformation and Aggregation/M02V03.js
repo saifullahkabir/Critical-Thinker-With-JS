ce3440e5ea4910fa4a1452af09f34261e6592fc0
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-04-29 01:35:28 +0600

    Sorting and flattening array examples

---
+++ JavaScript Data Transformation and Aggregation/M02V03.js
@@ -0,0 +1,26 @@
+//* sort
+const numbers = [40, 100, 1, 5, 25, 10];
+const fruits = ["Banana", "apple", "Cherry", "date"];
+
+// const sortedNumber = numbers.sort((a, b) => a - b);
+
+fruits.sort((a, b) => a.localeCompare(b));
+
+// console.log(fruits);
+
+//* Nested array flat
+const arr = [1, 2, 3, [4, 5, [6, 7, [8, 9, [10, 11]]]]];
+
+const flatArray = arr.flat(Infinity);
+
+// console.log(flatArray);
+
+const tagsFromPosts = [
+    ["javascript", "react", "css"],
+    ["node", "express"],
+    ["css", "html", "react"],
+];
+
+const filterTags = [...new Set(tagsFromPosts.flat())];
+
+console.log(filterTags);
