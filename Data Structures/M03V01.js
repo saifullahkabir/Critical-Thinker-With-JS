66ef40325acd9f7900983e7506065df4c1642c40
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-04-30 16:57:47 +0600

    class constructor and method explore

---
+++ Data Structures/M03V01.js
@@ -0,0 +1,31 @@
+//* Stateless vs Statefull
+//* function : stateless
+//* object : statefull
+
+// const counter = (amount) => {
+//     let count = 0;
+
+//     count = count + amount;
+
+//     return count;
+// }
+
+// console.log(counter(3));
+// console.log(counter(2));
+
+const counter = {
+  count: 0,
+
+  add(amount) {
+    this.count = this.count + amount;
+  },
+
+  print() {
+    console.log(this.count);
+  },
+};
+
+counter.add(3);
+counter.add(2);
+
+counter.print();
